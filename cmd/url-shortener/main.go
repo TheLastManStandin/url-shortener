4dc49a94d1fb32f4feafd0c94b9f61af052b0818
cahisa <cahisabeat@gmail.com>
2026-07-22 20:49:08 +0300

    first

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"log/slog"
+	"os"
+	"url-shortener/internal/config"
+	"url-shortener/internal/storage/sqlite"
+)
+
+const (
+	envLocal = "local"
+	envProd  = "prod"
+	envDev   = "dev"
+)
+
+func main() {
+	cfg := config.MustLoad()
+	fmt.Println(cfg)
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
+
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Errorf("failed to init storage: %v", err)
+	}
+
+	// todo : init router
+
+	// todo : run server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+				Level: slog.LevelDebug,
+			}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+				Level: slog.LevelDebug,
+			}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+				Level: slog.LevelInfo,
+			}),
+		)
+	default:
+		panic("logger environment not supported")
+	}
+
+	return log
+}
